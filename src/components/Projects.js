b87b8a31de8bc30d3c963f4ca660f1ac11ce29d3
Latifa <latifaabdullayeva@gmail.com>
2020-12-07 23:48:20 +0100

    Adds projects section

---
+++ src/components/Projects.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+import Link from "@material-ui/core/Link";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import {makeStyles} from '@material-ui/core/styles';
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import autonom from "../images/projects/autonom.jpg"
+import musicorum from "../images/projects/musicorum.jpeg"
+import penny from "../images/projects/penny.jpg"
+import abdullayeva from "../images/myPhotoBlackWhite.png"
+
+const useStyles = makeStyles({
+    root: {
+        margin: '1rem',
+    },
+    media: {
+        paddingTop: '100%',
+    }
+});
+
+function Projects() {
+    const classes = useStyles();
+
+    return (
+        <Grid container style={{textAlign: 'center', padding: '2rem'}}>
+            <Grid xs={12} style={{marginBottom: '1rem'}}>
+                <Breadcrumbs aria-label="breadcrumb">
+                    <NavLink to="/abdullayeva" style={{textDecoration: 'none'}}>
+                        <Link color="textSecondary">Home</Link>
+                    </NavLink>
+                    <Link color="textPrimary" aria-current="page"
+                    >Projects</Link>
+                </Breadcrumbs>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} style={{justify: 'space-between', marginBottom: '1rem'}}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        {/*<Link to="/tracks" style={{textDecoration: 'none', color: 'black'}}>*/}
+                        <CardMedia className={classes.media}
+                                   image={autonom}
+                                   src={autonom}
+                        />
+                        <GridListTileBar title={"Autonomous System"}
+                                         subtitle={<span>Master's thesis project</span>}/>
+                        {/*</Link>*/}
+                    </CardActionArea>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} style={{justify: 'space-between', marginBottom: '1rem'}}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        {/*<Link to="/tracks" style={{textDecoration: 'none', color: 'black'}}>*/}
+                        <CardMedia className={classes.media}
+                                   image={musicorum}
+                                   src={musicorum}
+                        />
+                        <GridListTileBar title={"Musicorum"}
+                                         subtitle={<span>Music web application </span>}/>
+                        {/*</Link>*/}
+                    </CardActionArea>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} style={{justify: 'space-between', marginBottom: '1rem'}}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        {/*<Link to="/tracks" style={{textDecoration: 'none', color: 'black'}}>*/}
+                        <CardMedia className={classes.media}
+                                   image={abdullayeva}
+                                   src={abdullayeva}
+                        />
+                        <GridListTileBar title={"Abdullayeva"}
+                                         subtitle={<span>Personal website </span>}/>
+                        {/*</Link>*/}
+                    </CardActionArea>
+                </Card>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} style={{justify: 'space-between', marginBottom: '1rem'}}>
+                <Card className={classes.root}>
+                    <CardActionArea>
+                        {/*<Link to="/tracks" style={{textDecoration: 'none', color: 'black'}}>*/}
+                        <CardMedia className={classes.media}
+                                   image={penny}
+                                   src={penny}
+                        />
+                        <GridListTileBar title={"Penny"}
+                                         subtitle={<span>Currency converter application </span>}/>
+                        {/*</Link>*/}
+                    </CardActionArea>
+                </Card>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default Projects;
+{/*<AutonomousSystem/>*/
+}
+{/*<Abdullayeva/>*/
+}
+{/*<Musicorum/>*/
+}
+{/*<Penny/>*/
+}
